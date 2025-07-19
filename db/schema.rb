9d57993429df2ecdd2a1c39ec47b4facff51db41
Nixxiva <sarahbustinera24@gmail.com>
2025-07-19 10:59:54 +0800

    Fixed postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2025_07_12_023756) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "stocks", force: :cascade do |t|
     t.string "symbol"
     t.integer "balance"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_stocks_on_user_id"
@@ -26,7 +29,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_12_023756) do
     t.integer "quantity"
     t.float "price"
     t.float "total_price"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_transactions_on_user_id"
