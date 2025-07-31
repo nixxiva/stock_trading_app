a31d88e5dfd27963401b08c1f2bc146c092cd0d5
Nixxiva <sarahbustinera24@gmail.com>
2025-07-31 17:33:54 +0800

    Fix design and functionalities for user management

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < ApplicationController
   layout 'admin'
   
   def index
+    @all_users = User.where(is_admin: false)
+    @pending_users = User.where(is_admin: false, is_approved: false)
   end
 
   def show
